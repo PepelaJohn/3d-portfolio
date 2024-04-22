b67e9d6d9670b7fae430ec67c0d24ad081f0ea1e
Pepela John <pepelajahy@gmail.com>
2024-04-22 14:13:02 +0300

    final commit

--- src/components/canvas/EarthCanvas.jsx
+++ src/components/canvas/EarthCanvas.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader";
+import { Suspense } from "react";
+const Earth = () => {
+  const earth = useGLTF("./planet/scene.gltf");
+  return (
+    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+  );
+};
 
 const EarthCanvas = () => {
   return (
-    <div>EarthCanvas</div>
-  )
-}
+    <Canvas
+      shadows
+      frameloop="demand"
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{ fov: 45, near: 0.1, far: 200, position: [-4, 3, 6] }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <ambientLight intensity={20} />
+        <OrbitControls autoRotate enableZoom={false} />
+        <Earth />
+      </Suspense>
+    </Canvas>
+  );
+};
 
-export default EarthCanvas
+export default EarthCanvas;
--- src/components/canvas/StarCanvas.jsx
+++ src/components/canvas/StarCanvas.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { Suspense, useRef } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  OrbitControls,
+  Points,
+  Preload,
+  PointMaterial,
+} from "@react-three/drei";
+import * as random from "maath/random/dist/maath-random.esm";
+const Stars = (props) => {
+  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
+  return (
+    <group>
+      <Points positions={sphere} stride={3} frustumCulled {...props}>
+        <PointMaterial
+          transparent
+          color="#f272c8"
+          size={0.002}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
+};
 
 const StarCanvas = () => {
   return (
-    <div>StarCanvas</div>
-  )
-}
+    <div className="w-full -z-10 h-auto absolute inset-0">
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <ambientLight intensity={1} />
+        <OrbitControls
+          makeDefault
+          autoRotate
+          autoRotateSpeed={0.1}
+          enablePan={false}
+          enableZoom={false}
+        />
+        <Suspense fallback={null}>
+          <Stars />
+          <Preload all />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+};
 
-export default StarCanvas
+export default StarCanvas;
